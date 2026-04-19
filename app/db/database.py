61666d1aeaa27ba9845d60e3afc7b21ded1cdbd1
Ayesha Cyril <ayeshacy@si-b7.nsk.21-school.ru>
2026-04-19 15:28:23 +0700

    upd docker

--- app/db/database.py
+++ app/db/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-engine = create_engine("sqlite:///./database.db")
+engine = create_engine("sqlite:///./data/database.db")
 
 SessionLocal = sessionmaker(bind=engine)
 
